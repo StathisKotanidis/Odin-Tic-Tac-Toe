63af0c7d6f8dce4444d797688d9044bd944b4b15
Stathis Kot <stathiskotanidhs@gmail.com>
2024-08-16 20:03:56 +0300

    started the gameboard section

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-}
 const startGameBtn = document.querySelector("#start-game-button");
 const startingScreen = document.querySelector(".starting-screen");
 const playersSectionScreen = document.querySelector(".players-section");
-startGameBtn.addEventListener("click", () => {
-  startingScreen.style.display = "none";
-  playersSectionScreen.style.display = "grid";
-});
-
-// Changing the marker everytime i press a left/right arrow
 const playerOneLeftArrowBtn = document.querySelector(".player-one-left");
 const playerOneRightArrowBtn = document.querySelector(".player-one-right");
 const playerTwoLeftArrowBtn = document.querySelector(".player-two-left");
@@ -20,7 +10,25 @@ const playerTwoMarker = document.querySelector("#player-two-marker");
 const showGameboardBtn = document.querySelector("#proceed-to-gameboard");
 const playerOneName = document.querySelector("#player-one-name");
 const playerTwoName = document.querySelector("#player-two-name");
+const gameBoardContainer = document.querySelector('.gameboard-container')
 
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+}
+
+let player1;
+let player2;
+
+const gameboard = [];
+
+startGameBtn.addEventListener("click", () => {
+  startingScreen.style.display = "none";
+  playersSectionScreen.style.display = "grid";
+});
+
+// Changing the marker everytime i press a left/right arrow
 playerOneLeftArrowBtn.addEventListener("click", () => {
   playerOneMarker.innerHTML === "X"
     ? (playerOneMarker.innerHTML = "O")
@@ -61,27 +69,22 @@ function updateButtonState() {
   }
 }
 
-let player1Name;
-let player2Name;
-let player1Marker;
-let player2Marker;
-let player1;
-let player2;
-
 /*disable the current content and enable the gameboard 
 plus assigning my data to my players objects */
-
 showGameboardBtn.addEventListener("click", (e) => {
   e.preventDefault();
   playersSectionScreen.style.display = "none";
 
-  player1Name = playerOneName.value;
-  player2Name = playerTwoName.value;
-  player1Marker = playerOneMarker.innerHTML;
-  player2Marker = playerTwoMarker.innerHTML;
+  let player1Name = playerOneName.value;
+  let player2Name = playerTwoName.value;
+  let player1Marker = playerOneMarker.innerHTML;
+  let player2Marker = playerTwoMarker.innerHTML;
 
   player1 = new Player(player1Name, player1Marker);
   player2 = new Player(player2Name, player2Marker);
+
+  gameBoardContainer.style.display = 'grid'
 });
 
-const gameboard = [];
+
+
