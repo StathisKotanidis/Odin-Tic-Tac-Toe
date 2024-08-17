7989e53ff82ac3f635a48f3c743321464fe9c405
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-08-17 12:25:06 +0300

    created the board dynamically and  made a winCondition function

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ const playerTwoMarker = document.querySelector("#player-two-marker");
 const showGameboardBtn = document.querySelector("#proceed-to-gameboard");
 const playerOneName = document.querySelector("#player-one-name");
 const playerTwoName = document.querySelector("#player-two-name");
-const gameBoardContainer = document.querySelector('.gameboard-container')
-const playerOneNamePlaceholder = document.querySelector('#player-one-name-placeholder')
-const playerTwoNamePlaceholder = document.querySelector('#player-two-name-placeholder')
-const playerOneMarkerPlaceholder = document.querySelector('#player-one-marker-placeholder')
-const playerTwoMarkerPlaceholder = document.querySelector('#player-two-marker-placeholder')
-
+const gameBoardContainer = document.querySelector(".gameboard-container");
+const playerOneNamePlaceholder = document.querySelector(
+  "#player-one-name-placeholder"
+);
+const playerTwoNamePlaceholder = document.querySelector(
+  "#player-two-name-placeholder"
+);
+const playerOneMarkerPlaceholder = document.querySelector(
+  "#player-one-marker-placeholder"
+);
+const playerTwoMarkerPlaceholder = document.querySelector(
+  "#player-two-marker-placeholder"
+);
+const gameBoard = document.querySelector(".gameboard");
 
 function Player(name, marker) {
   this.name = name;
@@ -87,14 +95,128 @@ showGameboardBtn.addEventListener("click", (e) => {
   player1 = new Player(player1Name, player1Marker);
   player2 = new Player(player2Name, player2Marker);
 
-  gameBoardContainer.style.display = 'grid'
+  gameBoardContainer.style.display = "grid";
 
   //Updating Players Names/Markes based on the input i chose in the previous section
-  playerOneNamePlaceholder.innerHTML = player1.name
-  playerTwoNamePlaceholder.innerHTML = player2.name
-  playerOneMarkerPlaceholder.innerHTML = player1.marker
-  playerTwoMarkerPlaceholder.innerHTML = player2.marker
+  playerOneNamePlaceholder.innerHTML = player1.name;
+  playerTwoNamePlaceholder.innerHTML = player2.name;
+  playerOneMarkerPlaceholder.innerHTML = player1.marker;
+  playerTwoMarkerPlaceholder.innerHTML = player2.marker;
+
+  //a function that checks all possible senarios of winning the gam
+  function winCondition() {
+    if (
+      gameboard[0] === player1.marker &&
+      gameBoard[1] === player1.marker &&
+      gameBoard[2] === player1.marker
+    ) {
+      console.log("Player 1 won!");
+    } else if (
+      gameboard[0] === player2.marker &&
+      gameBoard[1] === player2.marker &&
+      gameBoard[2] === player2.marker
+    ) {
+      console.log("Player 2 won!");
+    } else if (
+      gameboard[3] === player1.marker &&
+      gameBoard[4] === player1.marker &&
+      gameBoard[5] === player1.marker
+    ) {
+      console.log("Player 1 won");
+    } else if (
+      gameboard[3] === player2.marker &&
+      gameBoard[4] === player2.marker &&
+      gameBoard[5] === player2.marker
+    ) {
+      console.log("Playe 2 won!");
+    } else if (
+      gameboard[6] === player1.marker &&
+      gameBoard[7] === player1.marker &&
+      gameBoard[8] === player1.marker
+    ) {
+      console.log("Player 1 won!");
+    } else if (
+      gameboard[6] === player2.marker &&
+      gameBoard[7] === player2.marker &&
+      gameBoard[8] === player2.marker
+    ) {
+      console.log("Player 2 won!");
+    } else if (
+      gameboard[0] === player1.marker &&
+      gameBoard[3] === player1.marker &&
+      gameBoard[6] === player1.marker
+    ) {
+      console.log("Player 1 won!");
+    } else if (
+      gameboard[0] === player2.marker &&
+      gameBoard[3] === player2.marker &&
+      gameBoard[6] === player2.marker
+    ) {
+      console.log("Player 2 won!");
+    } else if (
+      gameboard[1] === player1.marker &&
+      gameBoard[4] === player1.marker &&
+      gameBoard[7] === player1.marker
+    ) {
+      console.log("Player 1 won!");
+    } else if (
+      gameboard[1] === player2.marker &&
+      gameBoard[4] === player2.marker &&
+      gameBoard[7] === player2.marker
+    ) {
+      console.log("Player 2 won!");
+    } else if (
+      gameboard[2] === player1.marker &&
+      gameBoard[5] === player1.marker &&
+      gameBoard[8] === player1.marker
+    ) {
+      console.log("player 1 won!");
+    } else if (
+      gameboard[2] === player2.marker &&
+      gameBoard[5] === player2.marker &&
+      gameBoard[8] === player2.marker
+    ) {
+      console.log("Player 2 won!");
+    } else if (
+      gameboard[0] === player1.marker &&
+      gameBoard[4] === player1.marker &&
+      gameBoard[8] === player1.marker
+    ) {
+      console.log("Player 1 won!");
+    } else if (
+      gameboard[0] === player2.marker &&
+      gameBoard[4] === player2.marker &&
+      gameBoard[8] === player2.marker
+    ) {
+      console.log("Player 2 won!");
+    } else if (
+      gameboard[2] === player1.marker &&
+      gameBoard[4] === player1.marker &&
+      gameBoard[6] === player1.marker
+    ) {
+      console.log("Player 1 won!");
+    } else if (
+      gameboard[2] === player2.marker &&
+      gameBoard[4] === player2.marker &&
+      gameBoard[6] === player2.marker
+    ) {
+      console.log("Player 2 won!");
+    } else {
+      console.log("It's a draw");
+    }
+  }
 });
 
+function createGameBoard() {
+  for (let i = 1; i <= 9; i++) {
+    let gameboardDiv = document.createElement("div");
+    gameboardDiv.innerHTML = `${i}`;
+    gameboardDiv.className = `gameboard-div div-${i}`;
+    gameboard.push(gameboardDiv);
+    gameBoard.appendChild(gameboardDiv);
+  }
+}
 
+createGameBoard();
 
+console.log(gameboard);
